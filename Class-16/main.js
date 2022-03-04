07d0d57dc8cba1cc9e57c0c08de481f651eb5ca3
MeeraNair07 <meera07nair@gmail.com>
2022-03-04 04:01:41 +0300

    levels

--- Class-16/main.js
+++ Class-16/main.js
@@ -1,1 +1,19 @@
 //Variables -> Data buckets -> Declaration, Assignment
+
+//Easy
+let num = 25;
+num -= 10;
+console.log(num);
+
+//Medium
+let inputedVal = document.querySelector('#danceDanceRevolution').value; //is an empty string since there isn't an input 
+inputedVal += 25;
+alert(inputedVal);
+
+//Hard
+const h1Holder = document.querySelector('h1');
+h1Holder.addEventListener('click', sum);
+
+function sum() {
+    console.log(num + Number(inputedVal))
+}
