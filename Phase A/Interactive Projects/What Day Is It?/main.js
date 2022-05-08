0dc00eb1ec6d90c08882d91bb56b5e3ae319fcbb
MeeraNair07 <meera07nair@gmail.com>
2022-05-08 23:41:55 +0300

    exploring inbuilt functions

--- Phase A/Interactive Projects/What Day Is It?/main.js
+++ Phase A/Interactive Projects/What Day Is It?/main.js
@@ -1,10 +1,10 @@
 document.getElementById('check').addEventListener('click', result)
 
 function result(){
-    const day = document.querySelector('#day').value
-    if (day == 'Sunday' || day == 'Saturday'){
-        document.querySelector('#answer').innerText = "It's the Weekend!"
-    } else if (day == 'Monday' || day == 'Tuesday' || day == 'Wednesday' || day == 'Thursday' ||day == 'Friday'){
+    const day = document.querySelector('#day').value.toLowerCase()
+    if (day == 'sunday' || day == 'saturday'){
+        document.querySelector('#answer').innerText = "It's the weekend!"
+    } else if (day == 'monday' || day == 'tuesday' || day == 'wednesday' || day == 'thursday' ||day == 'Friday'){
         document.querySelector('#answer').innerText = "Oh nu, week day!"
     } else {
         alert("Please enter a day of the week!")
